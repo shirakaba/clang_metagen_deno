ba54eed6cba42b9ac4bc3fa6ac680357f8d6327f
Dj <43033058+DjDeveloperr@users.noreply.github.com>
2023-02-25 22:52:17 +0530

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -1,12 +1,14 @@
 export {
   CXEvalResultKind,
   CXVisitorResult,
-} from "https://deno.land/x/libclang@1.0.0-beta.4/include/typeDefinitions.ts";
+} from "https://deno.land/x/libclang@1.0.0-beta.8/include/typeDefinitions.ts";
 export {
   CXIndex,
   CXFile,
   CXCursor,
   CXCursorKind,
+  CXObjCPropertyAttrKind,
   type CXType,
-} from "https://deno.land/x/libclang@1.0.0-beta.4/mod.ts";
-export { CXChildVisitResult } from "https://deno.land/x/libclang@1.0.0-beta.4/include/typeDefinitions.ts";
+  type AvailabilityEntry,
+} from "https://deno.land/x/libclang@1.0.0-beta.8/mod.ts";
+export { CXChildVisitResult } from "https://deno.land/x/libclang@1.0.0-beta.8/include/typeDefinitions.ts";
