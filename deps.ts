ff72321ab0a2c65d913395c73ff41083fbca8516
shirakaba <14055146+shirakaba@users.noreply.github.com>
2023-02-25 19:27:49 +0900

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
 export {
   CXEvalResultKind,
   CXVisitorResult,
-} from "https://deno.land/x/libclang@1.0.0-beta.3/include/typeDefinitions.ts";
+} from "https://deno.land/x/libclang@1.0.0-beta.4/include/typeDefinitions.ts";
 export {
   CXIndex,
   CXFile,
   CXCursor,
   CXCursorKind,
   type CXType,
-} from "https://deno.land/x/libclang@1.0.0-beta.3/mod.ts";
-export { CXChildVisitResult } from "https://deno.land/x/libclang@1.0.0-beta.3/include/typeDefinitions.ts";
+} from "https://deno.land/x/libclang@1.0.0-beta.4/mod.ts";
+export { CXChildVisitResult } from "https://deno.land/x/libclang@1.0.0-beta.4/include/typeDefinitions.ts";
